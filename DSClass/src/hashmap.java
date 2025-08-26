6184376c1b41fb591c8697dcec67b4ab695c02ee
Saket-Nandan <saketnandan15@gamil.com>
2025-08-26 19:32:06 +0530

    normal update

--- DSClass/src/hashmap.java
+++ DSClass/src/hashmap.java
@@ -1,4 +1,41 @@
 package DSClass.src;
 
+import java.util.HashMap;
+
 public class hashmap {
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+
+        //Insertion
+        map.put("India", 120);
+        map.put("US", 30);
+        map.put("China", 110);
+
+        System.out.println(map);
+
+        //search
+        if(map.containsKey("China")){
+            System.out.println("Key is present in the map");
+        }else{
+            System.out.println("Key is not present in the map");
+        }
+
+//        System.out.println(map.get("China"));
+//        System.out.println(map.get("INdo"));
+//
+//        for(Map.Entry<String, Integer> e: map.entrySet()){
+//            System.out.println(e.getKey());
+//            System.out.println(e.getValue());
+//        }
+//
+//        Set<String> keys = map.keySet();
+//        for(String key : keys){
+//            System.out.println(key + " " + map.get(key));
+//        }
+
+        map.remove("China");
+        System.out.println(map);
+
+
+    }
 }

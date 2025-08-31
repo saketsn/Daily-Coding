29b2205056c40bdeca14097d4196fd8b78e68709
Saket-Nandan <saketnandan15@gmail.com>
2025-08-31 18:38:36 +0530

    BFS

--- Youtube/Graph/Classroom.java
+++ Youtube/Graph/Classroom.java
@@ -1,6 +1,8 @@
 package Youtube.Graph;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Classroom {
     static class Edge{
@@ -8,10 +10,10 @@ public class Classroom {
         int dest;
         int wt;
 
-        public Edge(int s, int d, int w){
+        public Edge(int s, int d){
             this.src = s;
             this.dest = d;
-            this.wt = w;
+
         }
     }
 
@@ -20,33 +22,67 @@ public class Classroom {
             graph[i] = new ArrayList<Edge>();
         }
 
-        graph[0].add(new Edge(0,2,2));
+        // Node 0 connections
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
 
-        graph[1].add(new Edge(1,2,10));
-        graph[1].add(new Edge(1,3,0));
+        // Node 1 connections
+        graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 3));
 
-        graph[2].add(new Edge(2,0,2));
-        graph[2].add(new Edge(2,1,10));
-        graph[2].add(new Edge(2,3,-1));
+        // Node 2 connections
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 4));
 
-        graph[3].add(new Edge(3,1,-1));
-        graph[3].add(new Edge(3,2,0));
+        // Node 3 connections
+        graph[3].add(new Edge(3, 1));
+        graph[3].add(new Edge(3, 4));
+        graph[3].add(new Edge(3, 5));
+
+        // Node 4 connections
+        graph[4].add(new Edge(4, 2));
+        graph[4].add(new Edge(4, 3));
+        graph[4].add(new Edge(4, 5));
+
+        // Node 5 connections
+        graph[5].add(new Edge(5, 3));
+        graph[5].add(new Edge(5, 4));
+        graph[5].add(new Edge(5, 6));
+
+        // Node 6 connections
+        graph[6].add(new Edge(6, 5));
 
 
     }
 
+    public static void bfs(ArrayList<Edge> graph[], int V){
+        Queue<Integer> q = new LinkedList<>();
+        boolean vis[] = new boolean[V];
+        q.add(0);
+
+        while(!q.isEmpty()){
+            int curr = q.remove();
+            if(vis[curr] == false){
+                System.out.print(curr+ " ");
+                vis[curr] = true;
+
+                for(int i=0; i< graph[curr].size(); i++){
+                    Edge e = graph[curr].get(i);
+                    q.add(e.dest);
+                }
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
-        int V = 4;
+        int V = 7;
 
         ArrayList<Edge> graph[] = new ArrayList[V];
 
         createGraph(graph);
+        bfs(graph, V);
+        System.out.println();
 
-        //print 2's neighbours
-        for(int i=0; i< graph[2].size(); i++){
-            Edge e = graph[2].get(i);
-            System.out.print(e.dest+ " , "+ e.wt);
-            System.out.println();
-        }
     }
 }

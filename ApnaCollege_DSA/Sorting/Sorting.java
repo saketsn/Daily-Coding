b9f2e503d9743105146b2fad4debde734699617d
Saket-Nandan <saketnandan15@gmail.com>
2025-11-05 22:19:28 +0530

    Merge sort

--- ApnaCollege_DSA/Sorting/Sorting.java
+++ ApnaCollege_DSA/Sorting/Sorting.java
@@ -1,13 +1,19 @@
 package ApnaCollege_DSA.Sorting;
 
-import java.util.Arrays;
-
 public class Sorting {
     public static void main(String[] args) {
-        int arr[] = {7, 8, 3, 2, 1};
+       // int arr[] = {7, 8, 3, 2, 1};
+        int arr[] = {6, 3, 9, 5, 2, 8};
         //bubbleSort(arr);
-        selectSort(arr);
-        System.out.println(Arrays.toString(arr));
+        //selectSort(arr);
+        mergeSort(arr, 0, arr.length-1);
+        print(arr);
+    }
+
+    public static void print(int[] arr){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
     }
 
     // Bubble Sort algo
@@ -45,8 +51,41 @@ public class Sorting {
 
     }
 
-    // Bucket sort
-    public static void insertionSort(int[] arr){
-        int n = arr.length;
+    // Merge sort
+    public static void mergeSort(int[] arr, int s, int e){
+        if(s >= e){
+            return;
+        }
+        int mid = s +(e-s)/2;
+        mergeSort(arr, s, mid);
+        mergeSort(arr, mid+1, e);
+        merge(arr, s, mid, e);
+
+    }
+    public static void merge(int[] arr, int s, int mid, int e){
+        int temp[] = new int[e-s+1];
+        int i= s;
+        int j= mid+1;
+        int k= 0;
+
+        while(i <= mid && j<= e){
+            if(arr[i] < arr[j]){
+                temp[k] = arr[i];
+                i++;
+            }else{
+                temp[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+        while(i <= mid){
+            temp[k++] = arr[i++];
+        }
+        while(j <= e){
+            temp[k++] = arr[j++];
+        }
+        for(k=0, i=s; k<temp.length; k++, i++){
+            arr[i] = temp[k];
+        }
     }
 }

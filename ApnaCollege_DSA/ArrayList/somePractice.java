1d0eb306adb0af585c1d6f4f99781a420eb2f3db
Saket-Nandan <saketnandan15@gmail.com>
2025-12-22 21:23:07 +0530

    String revers

---
+++ ApnaCollege_DSA/ArrayList/somePractice.java
@@ -0,0 +1,10 @@
+package ApnaCollege_DSA.ArrayList;
+
+import java.util.*;
+
+public class somePractice {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+    }
+}
---
+++ Daily_Array_String_Code/ReversString.java
@@ -0,0 +1,31 @@
+/*
+Reverse a string without using built-in functions
+
+ */
+package Daily_Array_String_Code;
+
+
+import java.util.Scanner;
+
+public class ReversString {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String :");
+        String str = sc.nextLine();
+        System.out.println("The String after revers is = "+ reverse(str));
+    }
+
+    public static String reverse(String str){
+        char[] chars = str.toCharArray();
+        int l=0, r= chars.length-1;
+        while(l<r){
+            char temp = chars[l];
+            chars[l] = chars[r];
+            chars[r] = temp;
+            l++;
+            r--;
+        }
+        String result = new String(chars);
+        return result;
+    }
+}

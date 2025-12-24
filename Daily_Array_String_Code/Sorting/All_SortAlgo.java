cebc1afba6a473fbe879a2a3186539ed3811b5ef
Saket-Nandan <saketnandan15@gmail.com>
2025-12-24 20:43:55 +0530

    more code

--- Daily_Array_String_Code/Sorting/All_SortAlgo.java
+++ Daily_Array_String_Code/Sorting/All_SortAlgo.java
@@ -51,6 +51,21 @@ public class All_SortAlgo {
 
     }
 
+    public static  void insertionSort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
     public static void mergeSort(int[] arr, int s, int e){
         if(s>=e) return ;
 
